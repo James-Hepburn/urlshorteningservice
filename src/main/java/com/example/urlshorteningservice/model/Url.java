a566757106fa2adccaea8d18673519de618d9be3
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-21 20:18:37 -0600

    Entity and repository

--- src/main/java/com/example/urlshorteningservice/model/Url.java
+++ src/main/java/com/example/urlshorteningservice/model/Url.java
@@ -1,4 +1,26 @@
 package com.example.urlshorteningservice.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Url {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String shortCode;
+
+    private String url;
+    private int accessCount;
+
+    public Url (String url, String shortCode) {
+        this.url = url;
+        this.shortCode = shortCode;
+        this.accessCount = 0;
+    }
 }
--- src/main/java/com/example/urlshorteningservice/repository/UrlRepository.java
+++ src/main/java/com/example/urlshorteningservice/repository/UrlRepository.java
@@ -1,4 +1,12 @@
 package com.example.urlshorteningservice.repository;
 
-public class UrlRepository {
+import com.example.urlshorteningservice.model.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UrlRepository extends JpaRepository <Url, Long> {
+    Optional <Url> findByShortCode (String shortCode);
 }
